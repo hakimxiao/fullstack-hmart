40bb1283bbfa80764113bb2f5818ce22c9cee8bb
hakimxiao <alhakimabusolihin@gmail.com>
2026-04-15 23:57:03 +0700

    Add CORS and Clerk middleware integration in the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import cors from "cors";
+
 import { ENV } from "./config/env";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
+
+app.use(cors({ origin: ENV.FRONTEND_URL }));
+
+app.use(clerkMiddleware()); // auth obj will be attached to the req
+app.use(express.json()); // parse JSON req body
+app.use(express.urlencoded({ extended: true })); // parses form data (like HTML forms)
 
 app.get("/", (req, res) => {
   res.json({
